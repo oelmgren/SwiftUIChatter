d8c9e52fbbe7a1931bad8306bb21fbaeff61a4e0
Andrew Elmgren <oelmgren@umich.edu>
2020-09-18 11:14:51 -0400

    Initial Commit

---
+++ demo_swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo_swiftUI
+//
+//  Created by Ollie Elmgren on 9/18/20.
+//  Copyright © 2020 Ollie Elmgren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
