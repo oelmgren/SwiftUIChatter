cbd04cca94a6f333cae5032f1956915a364068cf
Andrew Elmgren <oelmgren@umich.edu>
2020-10-02 12:39:19 -0400

    first commit

---
+++ demo_swiftUI/Chatt.swift
@@ -0,0 +1,9 @@
+//
+//  Chatt.swift
+//  demo_swiftUI
+//
+//  Created by Ollie Elmgren on 9/18/20.
+//  Copyright © 2020 Ollie Elmgren. All rights reserved.
+//
+
+import Foundation
---
+++ demo_swiftUI/PostView.swift
@@ -0,0 +1,21 @@
+//
+//  PostView.swift
+//  demo_swiftUI
+//
+//  Created by Ollie Elmgren on 9/24/20.
+//  Copyright © 2020 Ollie Elmgren. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
